a2799e30a07d42f35b96580ad6a1e00f7a14bca0
Miklos Lang <miklos.lang@accedo.tv>
2018-06-11 18:11:28 +0200

    Initial Commit

---
+++ TechTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechTest
+//
+//  Created by Miklos Lang on 2018. 06. 11..
+//  Copyright © 2018. Miklos Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
